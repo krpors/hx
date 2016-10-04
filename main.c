482daa569496caa64caf907cd7d35717facb4618
Kevin Pors <krpors@gmail.com>
2016-10-04 16:36:43 +0200

    More screwing about.
    Colorized rendering works sort of. The 'editor' doesn't do pagination
    yet. So that was a relatively easy part. Next is figuring out how to
    make an editor actually work...

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -11,12 +12,38 @@
 #include <sys/ioctl.h>
 
 // Declarations.
+
+/**
+ * This struct contains internal information of the state of the editor.
+ */
+struct editor {
+	int cursor_x;    // Cursor x pos
+	int cursor_y;    // Cursor y pos
+	int screen_rows; // amount of screen rows after init
+	int screen_cols; // amount of screen columns after init
+
+	bool           dirty;          // whether the buffer is modified
+	char*          filename;       // the filename currently open
+	unsigned char* contents;       // the file's contents
+	int            content_length; // length of the contents
+
+	char statusmessage[80];  // status message
+};
+
+// Functions:
 void enable_raw_mode();
 void disable_raw_mode();
 int read_key();
+void process_keypress(struct editor* ec);
+
+struct editor* editor_init();
+void           editor_free(struct editor* ec);
+
+// Global editor config.
+struct editor* ec;
 
 // Terminal IO settings. Used to reset it when exiting to prevent terminal
-// data garbling.
+// data garbling. Declared global since we require it in the atexit() call.
 struct termios orig_termios;
 
 // Key enumerations
@@ -27,12 +54,12 @@ enum KEY_CODES {
 
 	// 'Virtual keys', i.e. not corresponding to terminal escape sequences
 	// or any other ANSI stuff. Merely to identify keys returned by read_key().
-	VKEY_UP      = 1000, // [A
-	VKEY_DOWN,           // [B
-	VKEY_RIGHT,          // [C
-	VKEY_LEFT,           // [D
-	VKEY_HOME,           // [H
-	VKEY_END,            // [F
+	KEY_UP      = 1000, // [A
+	KEY_DOWN,           // [B
+	KEY_RIGHT,          // [C
+	KEY_LEFT,           // [D
+	KEY_HOME,           // [H
+	KEY_END,            // [F
 };
 
 void enable_raw_mode() {
@@ -65,7 +92,7 @@ void enable_raw_mode() {
 	raw.c_cc[VTIME] = 1;
 
     // put terminal in raw mode after flushing
-	int x = tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	int x = tcsetattr(STDIN_FILENO, TCSANOW, &raw);
 	if (x != 0) {
 		perror("Unable to set terminal attributes");
 		exit(1);
@@ -73,9 +100,10 @@ void enable_raw_mode() {
 }
 
 void disable_raw_mode() {
+	editor_free(ec);
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 
-	printf("\x1b[7mThank you for using hx!\x1b[0K");
+	printf("\n\x1b[7mThank you for using hx!\x1b[0K");
 	fflush(stdout);
 }
 
@@ -86,19 +114,15 @@ void clear_screen() {
 	}
 }
 
-void set_cursor_pos(uint8_t x, uint8_t y) {
-	printf("\x1b[%d;%dH", x, y);
-}
-
 void move_cursor(int dir) {
 	static const int LEN = 4;
 	char cruft[LEN];
 
 	switch (dir) {
-	case VKEY_UP:    strncpy(cruft, "\x1b[1A", LEN); break;
-	case VKEY_RIGHT: strncpy(cruft, "\x1b[1C", LEN); break;
-	case VKEY_DOWN:  strncpy(cruft, "\x1b[1B", LEN); break;
-	case VKEY_LEFT:  strncpy(cruft, "\x1b[1D", LEN); break;
+	case KEY_UP:    strncpy(cruft, "\x1b[1A", LEN); break;
+	case KEY_RIGHT: strncpy(cruft, "\x1b[1C", LEN); break;
+	case KEY_DOWN:  strncpy(cruft, "\x1b[1B", LEN); break;
+	case KEY_LEFT:  strncpy(cruft, "\x1b[1D", LEN); break;
 	default: return;
 	}
 
@@ -119,6 +143,11 @@ bool get_window_size(int* rows, int* cols) {
 	return true;
 }
 
+/**
+ * This buffer contains the character sequences to render the current
+ * 'screen'. The buffer is changed as a whole, then written to the screen
+ * in one go to prevent 'flickering' in the terminal.
+ */
 struct buffer {
 	char* contents;
 	int len;
@@ -142,7 +171,7 @@ struct buffer* buffer_create() {
 /**
  * Deletes the buffer's contents, and the buffer itself.
  */
-void buffer_delete(struct buffer* buf) {
+void buffer_free(struct buffer* buf) {
 	free(buf->contents);
 	free(buf);
 }
@@ -153,6 +182,8 @@ void buffer_delete(struct buffer* buf) {
  * 1 from the `len', to discard the null terminator character.
  */
 void buffer_append(struct buffer* buf, const char* what, size_t len) {
+	assert(what != NULL);
+
 	// reallocate the contents with more memory, to hold 'what'.
 	char* new = realloc(buf->contents, buf->len + len);
 	if (new == NULL) {
@@ -167,9 +198,21 @@ void buffer_append(struct buffer* buf, const char* what, size_t len) {
 }
 
 /**
- * Opens a file denoted by `filename', or exit if the file cannot be opened.
+ * Draws (writes) the buffer to the screen.
  */
-void openfile(const char* filename) {
+void buffer_draw(struct buffer* buf) {
+	if (write(STDOUT_FILENO, buf->contents, buf->len) == -1) {
+		perror("Can't write buffer");
+		exit(1);
+	}
+}
+
+/**
+ * Opens a file denoted by `filename', or exit if the file cannot be opened.
+ * The editor struct is used to contain the contents and other metadata
+ * about the file being opened.
+ */
+void editor_openfile(struct editor* ec, const char* filename) {
 	FILE* fp = fopen(filename, "rb");
 	if (fp == NULL) {
 		perror("Opening file");
@@ -197,31 +240,11 @@ void openfile(const char* filename) {
 		exit(1);
 	}
 
-	// write formatted data to a string
-	struct buffer* buf = buffer_create();
+	ec->filename = malloc(strlen(filename));
+	strncpy(ec->filename, filename, strlen(filename));
+	ec->contents = contents;
+	ec->content_length = size;
 
-	// Variable used to check how many bytes were written using sprintf,
-	// excluding the terminating null byte.
-	int bytes_written = 0;
-
-	for(int i = 0; i < size; i++) {
-		char str[1000] = {0};
-
-		if (isprint(contents[i])) {
-			bytes_written = sprintf(str, "%09x: %c = %02x\n", i, contents[i], contents[i]);
-		} else {
-			bytes_written = sprintf(str, "%09x: \x1b[31;47m.\x1b[0m = %02x\n", i, contents[i]);
-		}
-
-		buffer_append(buf, str, bytes_written);
-	}
-	if (write(STDOUT_FILENO, buf->contents, buf->len) == -1) {
-		perror("Unable to write to stdout");
-		exit(1);
-	}
-
-	buffer_delete(buf);
-	free(contents);
 	fclose(fp);
 }
 
@@ -229,6 +252,8 @@ void openfile(const char* filename) {
  * Reads keypresses from stdin, and processes them accordingly. Escape sequences
  * will be read properly as well (e.g. DEL will be the bytes 0x1b, 0x5b, 0x33, 0x7e).
  * The returned integer will contain either one of the enum values, or the key pressed.
+ *
+ * read_key() will only return the correct key code.
  */
 int read_key() {
 	char c;
@@ -249,115 +274,153 @@ int read_key() {
 		// For instance, a single Escape key only produces a single 0x1b char.
 		// A delete key produces 0x1b 0x5b 0x33 0x7e.
 		if (read(STDIN_FILENO, seq, 1) == 0) {
-			printf("Just an escape!\n");
 			return KEY_ESC;
 		}
 		if (read(STDIN_FILENO, seq + 1, 1) == 0) {
-			printf("Just an escape, 2!\n");
 			return KEY_ESC;
 		}
 
 		if (seq[0] == '[') {
 			switch (seq[1]) {
-			case 'A':
-				move_cursor(VKEY_UP);
-				return VKEY_UP;
-			case 'B':
-				move_cursor(VKEY_DOWN);
-				return VKEY_DOWN;
-			case 'C':
-				move_cursor(VKEY_RIGHT);
-				return VKEY_RIGHT;
-			case 'D':
-				move_cursor(VKEY_LEFT);
-				return VKEY_LEFT;
+			case 'A': return KEY_UP;
+			case 'B': return KEY_DOWN;
+			case 'C': return KEY_RIGHT;
+			case 'D': return KEY_LEFT;
+			case 'H': return KEY_HOME;
+			case 'F': return KEY_END;
 			}
 		}
-
-		break;
-	case KEY_CTRL_Q:
-		exit(0);
-		break;
-	default:
-		//write(STDOUT_FILENO, &c, 1);
-		//printf("%x\n", (unsigned char)c);
 		break;
 	}
 
+	//write(STDOUT_FILENO, &c, 1);
+	//printf("%x\n", (unsigned char)c);
 	return c;
 }
 
-/**
- * Simply prints the file's contents as hex.
- */
-void print_hex(const char* filename) {
-	FILE* fp = fopen(filename, "rb");
-	if (fp == NULL) {
-		perror("Unable to open file");
-		exit(EXIT_FAILURE);
-	}
+void render_contents(struct editor* e, struct buffer* b) {
+	char address[80]; // the address e.g. 000000040
+	char hex[39];     // hex characters e.g. 654d 0d0a
+	char asc[16];     // ascii equivalents of the hex
 
-	char full[16 + 1];  // e.g. 'MIT License..Cop\0'
-	char asci[ 1 + 1];  // e.g. 'a\0'
-	char chex[ 2 + 1];  // e.g. '4f\0'
-
-	int c;
 	int offset;
-	for (offset = 0; (c = fgetc(fp)) != EOF; offset++) {
+	for (offset = 0; offset < e->content_length; offset++) {
 		if (offset % 16 == 0) {
-			printf("%07x: ", offset);
-			memset(&full, 0, sizeof(full));
+			int bwritten = snprintf(address, sizeof(address), "\e[0;33m%09x\e[0m:", offset);
+			buffer_append(b, address, bwritten);
+			memset(asc, 0, sizeof(asc));
 		}
 
-		if (isprint(c)) {
-			snprintf(asci, 2, "%c", (unsigned char) c);
+		char narf[2 + 1];
+		int c = sprintf(narf, "%02x", (unsigned char) e->contents[offset]);
+
+		if (isprint(e->contents[offset])) {
+			asc[offset % 16] = e->contents[offset];
 		} else {
-			snprintf(asci, 2, ".");
-		}
-		// concat it
-		strcat(full, asci);
-
-		snprintf(chex, 3, "%02x", (unsigned char) c);
-		printf("%s", chex);
-
-		if ((offset+1) % 2 == 0) {
-			printf(" ");
+			asc[offset % 16] = '.';
 		}
 
+		if (offset % 2 == 0) {
+			buffer_append(b, " ", 1);
+		}
+
+		buffer_append(b, narf, c);
 		if ((offset+1) % 16 == 0) {
-			// TODO: print the string of chars.
-			printf(" %s\n",full);
-			//printf("\n");
+			buffer_append(b, "\e[1;32m", 8);
+			buffer_append(b, "  ", 2);
+			buffer_append(b, asc, strlen(asc));
+			buffer_append(b, "\r\n", 2);
 		}
 	}
-
-	// calc offset remainder, to pad some leftover hexes.
-	int leftover_bytes = (16 - offset % 16);
-	printf("\n\nPadding chars amount = %d\n", leftover_bytes);
-	if (leftover_bytes > 0) {
-		printf("%s\n", full);
-	}
-
-	fclose(fp);
 }
 
+/**
+ * Refreshes the screen. It uses a temporary buffer to write everything that's
+ * eligible for display to an internal buffer, and then 'draws' it to the screen
+ * in one call.
+ */
+void refresh_screen(struct editor* ec) {
+	struct buffer* b = buffer_create();
+
+	buffer_append(b, "\x1b[H", 3); // move the cursor top left
+
+	render_contents(ec, b);
+
+	// Move down to write the status message.
+	char buf[20];
+	int chars_written = snprintf(buf, 20, "\x1b[%d;0H", ec->screen_rows - 1);
+	buffer_append(b, buf, chars_written);
+	// Change the color
+	buffer_append(b, "\x1b[37m", 5);
+	buffer_append(b, ec->statusmessage, strlen(ec->statusmessage));
+	// Clear until the end of the line to the right
+	buffer_append(b, "\x1b[0m", 4);
+
+	buffer_draw(b);
+	buffer_free(b);
+}
+
+/**
+ * Processes a keypress accordingly.
+ */
+void process_keypress(struct editor* ec) {
+	int c = read_key();
+	if (c == KEY_CTRL_Q) {
+		exit(0);
+	} else if (c == KEY_UP) {
+	} else if (c == KEY_DOWN) {
+	} else if (c == KEY_RIGHT) {
+	} else if (c == KEY_LEFT) {
+	} else if (c == KEY_HOME) {
+	} else if (c == KEY_END) {
+	}
+}
+
+/**
+ * Initializes editor struct with some default values.
+ */
+struct editor* editor_init() {
+	struct editor* ec = malloc(sizeof(struct editor));
+
+	ec->cursor_x = 0;
+	ec->cursor_y = 0;
+	ec->filename = NULL;
+	ec->contents = NULL;
+	ec->content_length = 0;
+	get_window_size(&(ec->screen_rows), &(ec->screen_cols));
+	strncpy(ec->statusmessage, "Welcome!", 80);
+
+	return ec;
+}
+
+void editor_free(struct editor* ec) {
+	free(ec->filename);
+	free(ec->contents);
+	free(ec);
+}
 
 int main(int argc, char* argv[]) {
-#if 0
 	if (argc != 2) {
 		fprintf(stderr, "expected arg\n");
 		exit(2);
 	}
 
-	openfile(argv[1]);
-	//print_hex(argv[1]);
-#endif
+	// Editor configuration passed around.
+	ec = editor_init();
+	editor_openfile(ec, argv[1]);
 
 	enable_raw_mode();
-
 	clear_screen();
+
 	while (true) {
-		read_key();
+		refresh_screen(ec);
+		process_keypress(ec);
 	}
+
+	editor_free(ec);
 	return EXIT_SUCCESS;
 }
+
+/*
+0000000: 65 AF EF AA CC AA BB AA FF  A.....A.A.
+*/
