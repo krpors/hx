e4db710398decf02f8b9400de718d93f79734b24
Kevin Pors <krpors@gmail.com>
2016-10-05 15:27:50 +0200

    Appending works, renders correctly.
    Status message vararg stuff.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <errno.h>
@@ -22,10 +23,10 @@ struct editor {
 	int screen_rows; // amount of screen rows after init
 	int screen_cols; // amount of screen columns after init
 
-	bool           dirty;          // whether the buffer is modified
-	char*          filename;       // the filename currently open
-	unsigned char* contents;       // the file's contents
-	int            content_length; // length of the contents
+	bool  dirty;          // whether the buffer is modified
+	char* filename;       // the filename currently open
+	char* contents;       // the file's contents
+	int   content_length; // length of the contents
 
 	char statusmessage[80];  // status message
 };
@@ -38,6 +39,7 @@ void process_keypress(struct editor* ec);
 
 struct editor* editor_init();
 void           editor_free(struct editor* ec);
+int            editor_statusmessage(struct editor* ec, const char* fmt, ...);
 
 // Global editor config.
 struct editor* ec;
@@ -102,8 +104,7 @@ void enable_raw_mode() {
 void disable_raw_mode() {
 	editor_free(ec);
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-
-	printf("\n\x1b[7mThank you for using hx!\x1b[0K");
+	printf("\x1b[2J\x1b[7mThank you for using hx!\x1b[0K\r\n");
 	fflush(stdout);
 }
 
@@ -232,7 +233,7 @@ void editor_openfile(struct editor* ec, const char* filename) {
 	// allocate memory for the buffer. No need for extra
 	// room for a null string terminator, since we're possibly
 	// reading binary data only anyway (which can contain 0x00).
-	unsigned char* contents = malloc(sizeof(unsigned char) * size);
+	char* contents = malloc(sizeof(char) * size);
 
 	if (fread(contents, size, 1, fp) <= 0) {
 		perror("Unable to read bytes");
@@ -244,8 +245,17 @@ void editor_openfile(struct editor* ec, const char* filename) {
 	strncpy(ec->filename, filename, strlen(filename));
 	ec->contents = contents;
 	ec->content_length = size;
+	editor_statusmessage(ec, "File opened: %s (%d bytes)", ec->filename, ec->content_length);
 
 	fclose(fp);
+}
+
+int editor_statusmessage(struct editor* e, const char* fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	int x = vsnprintf(e->statusmessage, sizeof(e->statusmessage), fmt, ap);
+	va_end(ap);
+	return x;
 }
 
 /**
@@ -298,39 +308,74 @@ int read_key() {
 	return c;
 }
 
+/**
+ * Renders the contents of the current state of the editor `e'
+ * to the buffer `b'.
+ *
+ * TODO: parameterize things like grouping of bytes and whatnot,
+ * correctly! I.e. malloc the padding size dynamically based on
+ * the width.
+ */
 void render_contents(struct editor* e, struct buffer* b) {
-	char address[80]; // the address e.g. 000000040
-	char hex[39];     // hex characters e.g. 654d 0d0a
-	char asc[16];     // ascii equivalents of the hex
+	static const int WIDTH    = 16;            // 16 bytes per row.
+	static const int GROUPING = 2;             // byte grouping.
+
+	char address[80];  // example: 000000040
+	char hex[ 2 + 1];  // example: 65
+	char asc[256 + 1]; // example: Hello.World!
+
+	// Counter to indicate how many chars have been written for the current
+	// row of data. This is used for later for padding, when the iteration
+	// is over, but there's still some ASCII to write.
+	int row_char_count = 0;
 
 	int offset;
 	for (offset = 0; offset < e->content_length; offset++) {
-		if (offset % 16 == 0) {
+		if (offset % WIDTH == 0) {
 			int bwritten = snprintf(address, sizeof(address), "\e[0;33m%09x\e[0m:", offset);
 			buffer_append(b, address, bwritten);
 			memset(asc, 0, sizeof(asc));
+			row_char_count = 0;
 		}
 
-		char narf[2 + 1];
-		int c = sprintf(narf, "%02x", (unsigned char) e->contents[offset]);
+		snprintf(hex, sizeof(hex), "%02x", (unsigned char) e->contents[offset]);
 
+		// Every iteration, set the ascii value in the buffer, until
+		// 16 bytes are set. This will be written later when the hex
+		// values are drawn to screen.
 		if (isprint(e->contents[offset])) {
-			asc[offset % 16] = e->contents[offset];
+			asc[offset % WIDTH] = e->contents[offset];
 		} else {
-			asc[offset % 16] = '.';
+			asc[offset % WIDTH] = '.';
 		}
 
-		if (offset % 2 == 0) {
+		if (offset % GROUPING == 0) {
 			buffer_append(b, " ", 1);
+			row_char_count++;
 		}
 
-		buffer_append(b, narf, c);
-		if ((offset+1) % 16 == 0) {
+		// First, write the hex value of the byte at the current offset.
+		buffer_append(b, hex, 2);
+		row_char_count += 2;
+
+		// If we reached the end of a 'row', start writing the ASCII equivalents
+		// of the 'row', in a different color. Then hit CRLF to go to the next line.
+		if ((offset+1) % WIDTH == 0) {
 			buffer_append(b, "\e[1;32m", 8);
 			buffer_append(b, "  ", 2);
 			buffer_append(b, asc, strlen(asc));
 			buffer_append(b, "\r\n", 2);
 		}
+	}
+
+	// Check remainder of the last offset. If its bigger than zero,
+	// we got a last line to write (ASCII only).
+	if (offset % WIDTH > 0) {
+		char padding[980]; // padding characters, to align the ASCII.
+		memset(padding, ' ', (WIDTH * 2) + (WIDTH / GROUPING) - row_char_count);
+		buffer_append(b, padding, (WIDTH * 2) + (WIDTH / GROUPING) - row_char_count);
+		buffer_append(b, "\e[1;32m  ", 10);
+		buffer_append(b, asc, strlen(asc));
 	}
 }
 
@@ -340,6 +385,8 @@ void render_contents(struct editor* e, struct buffer* b) {
  * in one call.
  */
 void refresh_screen(struct editor* ec) {
+	char buf[32]; // temp buffer for snprintf.
+	int bw; // bytes written by snprintf.
 	struct buffer* b = buffer_create();
 
 	buffer_append(b, "\x1b[H", 3); // move the cursor top left
@@ -347,17 +394,30 @@ void refresh_screen(struct editor* ec) {
 	render_contents(ec, b);
 
 	// Move down to write the status message.
-	char buf[20];
-	int chars_written = snprintf(buf, 20, "\x1b[%d;0H", ec->screen_rows - 1);
-	buffer_append(b, buf, chars_written);
+	bw = snprintf(buf, 20, "\x1b[%d;0H", ec->screen_rows);
+	buffer_append(b, buf, bw);
 	// Change the color
-	buffer_append(b, "\x1b[37m", 5);
+	buffer_append(b, "\x1b[0;30;47m", 10);
 	buffer_append(b, ec->statusmessage, strlen(ec->statusmessage));
 	// Clear until the end of the line to the right
-	buffer_append(b, "\x1b[0m", 4);
+	buffer_append(b, "\x1b[0K", 4);
+
+	// Position cursor.
+	bw = snprintf(buf, sizeof(buf), "\x1b[%d;%dH", 0, 0);
+	buffer_append(b, buf, bw);
 
 	buffer_draw(b);
 	buffer_free(b);
+}
+
+void editor_insert(struct editor* ec, char x) {
+	// We are inserting a single character. Reallocate memory to contain
+	// this extra byte.
+	ec->contents = realloc(ec->contents, ec->content_length + 1);
+	// Set the last allocated index to the character x.
+	ec->contents[ec->content_length] = x;
+	// Increase the content length since we inserted a character.
+	ec->content_length++;
 }
 
 /**
@@ -373,6 +433,8 @@ void process_keypress(struct editor* ec) {
 	} else if (c == KEY_LEFT) {
 	} else if (c == KEY_HOME) {
 	} else if (c == KEY_END) {
+	} else {
+		editor_insert(ec, (char) c);
 	}
 }
 
@@ -388,7 +450,6 @@ struct editor* editor_init() {
 	ec->contents = NULL;
 	ec->content_length = 0;
 	get_window_size(&(ec->screen_rows), &(ec->screen_cols));
-	strncpy(ec->statusmessage, "Welcome!", 80);
 
 	return ec;
 }
