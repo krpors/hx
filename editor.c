727bdbb40ef172204f35873bc7a7dc92c1f7fefd
Mohamad Barbar <mbarbar@runbox.com>
2017-11-25 20:35:44 +1100

    Initialise field "dirty" in editor_init.

--- editor.c
+++ editor.c
@@ -1201,6 +1201,7 @@ struct editor* editor_init() {
 	e->filename = NULL;
 	e->contents = NULL;
 	e->content_length = 0;
+	e->dirty = false;
 
 	memset(e->status_message, '\0', sizeof(e->status_message));
 
